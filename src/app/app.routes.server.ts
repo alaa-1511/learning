88f604677cb072ba2553c95505fb4e91f17f0319
alaa-1511 <alaabdljll@gmail.com>
2026-01-21 19:54:50 +0200

    test

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,16 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  } ,
+  
+  {
+   path: 'article/:id',
+    renderMode: RenderMode.Server
+
+  },
+  {
+   path: 'course-details/:type/:id',
+    renderMode: RenderMode.Server
+
+  },
 ];
